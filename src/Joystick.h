dacafe6dcd3926fd98d5f15d2a85ed12722391a0
makkii_bcr <tonyu.makkii@gmail.com>
2018-09-29 22:20:10 +0900

    Fix linefeed code 3

--- src/Joystick.h
+++
@@ -1,9 +0,0 @@
-/********************************/ #ifndef JOYSTICK_H /*****************************/
-#define JOYSTICK_H
-
-#include <windows.h>
-#pragma comment(lib, "winmm.lib")
-
-BOOL Joystick_getOn(DWORD id);
-
-/*************************************/ #endif /**********************************/
